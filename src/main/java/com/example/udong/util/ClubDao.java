8722b4a863aa6cb48adba8b4d856b14aef50ee64
chaelin1211 <cofls749@naver.com>
2021-01-07 07:54:08 +0900

    Make Club getLIstJSON

--- src/main/java/com/example/udong/util/ClubDao.java
+++ src/main/java/com/example/udong/util/ClubDao.java
@@ -1,42 +1,76 @@
 package com.example.udong.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.example.udong.repository.H2DB;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClubDao {
-    
-    @Autowired
-    private SqlSessionTemplate sqlSession;
 
-    public Object getList(String sqlMapId, Object dataMap) {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public JSONArray getListJSON(Object dataMap){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		JSONArray jsonArray = new JSONArray();
+		try{
+			con = H2DB.getConnection();
+			String sql = "SELECT * FROM CLUB";
+			pstmt = con.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while(rs.next()){
+				JSONObject obj = new JSONObject();
+				obj.put("CLUB_NUM", rs.getString("CLUB_NUM");
+				obj.put("CLUB_NAME", rs.getString("CLUB_NAME");
+				obj.put("INTRO", rs.getString("INTRO");
+				obj.put("AREA_NAME", rs.getString("AREA_NAME");
+				obj.put("CONTENT", rs.getString("CONTENT");
+				obj.put("ID", rs.getString("ID");
+				obj.put("INTEREST", rs.getString("INTEREST");
+				obj.put("RECOMMEND", rs.getString("RECOMMEND");
+				jsonArray.add(obj);
+			}
+
+		}
+	}
+
+	public Object getList(String sqlMapId, Object dataMap) {
 		Object resultObject = sqlSession.selectList(sqlMapId, dataMap);
 
 		return resultObject;
-    }
+	}
 
-    public Object getObject(String sqlMapId, Object dataMap) {
+	public Object getObject(String sqlMapId, Object dataMap) {
 		Object resultObject = sqlSession.selectOne(sqlMapId, dataMap);
-		
+
 		return resultObject;
 	}
 
 	public Object saveObject(String sqlMapId, Object dataMap) {
 		Integer resultObject = sqlSession.insert(sqlMapId, dataMap);
-		
+
 		return resultObject;
 	}
 
 	public Object updateObject(String sqlMapId, Object dataMap) {
 		Integer resultObject = sqlSession.update(sqlMapId, dataMap);
-		
+
 		return resultObject;
 	}
 
 	public Object deleteObject(String sqlMapId, Object dataMap) {
-		
-		Integer resultObject = sqlSession.delete(sqlMapId,dataMap);
+
+		Integer resultObject = sqlSession.delete(sqlMapId, dataMap);
 		return resultObject;
 	}
 }
