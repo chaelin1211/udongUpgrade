c7de18d59c432ff63cfbd123c150cc3f2c93c9a4
chaelin1211 <cofls749@naver.com>
2021-01-07 06:22:47 +0900

    Create H2DB.java File

---
+++ src/main/java/com/example/udong/repository/H2DB.java
@@ -0,0 +1,57 @@
+package com.example.udong.repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public interface H2DB {
+    // 드라이버 클래스 이름
+    public static final String DRIVER_NAME = "org.h2.Driver";
+
+    // DB 연결
+    public static final String URL = "jdbc:h2:~/test";
+    public static final String USER = "";
+    public static final String PWD = "";
+
+    // DB 연결 메소드
+    public static Connection getConnection() {
+        // 1. 드라이버 로딩
+        Connection con = null;
+
+        // 2. DB 연결
+        con = DriverManager.getConnection(URL, USER, PWD);
+
+        return con;
+    }
+
+    // JDBC 닫기
+    public static void closeJDBC(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
