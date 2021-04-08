fe8d1371998748977748df4c1668640ae857cff2
chaelin1211 <cofls749@naver.com>
2021-04-09 02:13:09 +0900

    club 테이블에 time 컬럼 추가

--- src/main/java/com/example/udong/util/ClubBean.java
+++ src/main/java/com/example/udong/util/ClubBean.java
@@ -21,38 +21,25 @@ public class ClubBean implements Serializable{
     String CONTENT;
     String EMAIL;
     String NICKNAME;
+    String TIME;
     Integer INTEREST_NUM;
     Integer MEMBER_COUNT;
 
-    @Override
-    public String toString() {
-        return "{" +
-            " CLUB_NUM='" + getCLUB_NUM() + "'" +
-            ", CLUB_NAME='" + getCLUB_NAME() + "'" +
-            ", INTRO='" + getINTRO() + "'" +
-            ", AREA_NUM='" + getAREA_NUM() + "'" +
-            ", CONTENT='" + getCONTENT() + "'" +
-            ", EMAIL='" + getEMAIL() + "'" +
-            ", NICKNAME='" + getNICKNAME() + "'" +
-            ", INTEREST_NUM='" + getINTEREST_NUM() + "'" +
-            ", MEMBER_COUNT='" + getMEMBER_COUNT() + "'" +
-            "}";
+
+    public ClubBean() {
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof ClubBean)) {
-            return false;
-        }
-        ClubBean clubBean = (ClubBean) o;
-        return Objects.equals(CLUB_NUM, clubBean.CLUB_NUM) && Objects.equals(CLUB_NAME, clubBean.CLUB_NAME) && Objects.equals(INTRO, clubBean.INTRO) && Objects.equals(AREA_NUM, clubBean.AREA_NUM) && Objects.equals(CONTENT, clubBean.CONTENT) && Objects.equals(EMAIL, clubBean.EMAIL) && Objects.equals(NICKNAME, clubBean.NICKNAME) && Objects.equals(INTEREST_NUM, clubBean.INTEREST_NUM) && Objects.equals(MEMBER_COUNT, clubBean.MEMBER_COUNT);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(CLUB_NUM, CLUB_NAME, INTRO, AREA_NUM, CONTENT, EMAIL, NICKNAME, INTEREST_NUM, MEMBER_COUNT);
+    public ClubBean(Integer CLUB_NUM, String CLUB_NAME, String INTRO, Integer AREA_NUM, String CONTENT, String EMAIL, String NICKNAME, String TIME, Integer INTEREST_NUM, Integer MEMBER_COUNT) {
+        this.CLUB_NUM = CLUB_NUM;
+        this.CLUB_NAME = CLUB_NAME;
+        this.INTRO = INTRO;
+        this.AREA_NUM = AREA_NUM;
+        this.CONTENT = CONTENT;
+        this.EMAIL = EMAIL;
+        this.NICKNAME = NICKNAME;
+        this.TIME = TIME;
+        this.INTEREST_NUM = INTEREST_NUM;
+        this.MEMBER_COUNT = MEMBER_COUNT;
     }
 
     public Integer getCLUB_NUM() {
@@ -111,6 +98,14 @@ public class ClubBean implements Serializable{
         this.NICKNAME = NICKNAME;
     }
 
+    public String getTIME() {
+        return this.TIME;
+    }
+
+    public void setTIME(String TIME) {
+        this.TIME = TIME;
+    }
+
     public Integer getINTEREST_NUM() {
         return this.INTEREST_NUM;
     }
@@ -126,65 +121,86 @@ public class ClubBean implements Serializable{
     public void setMEMBER_COUNT(Integer MEMBER_COUNT) {
         this.MEMBER_COUNT = MEMBER_COUNT;
     }
-    
 
-    public ClubBean() {
-    }
-
-    public ClubBean(String CLUB_NAME, String NICKNAME, String INTRO, Integer AREA_NUM, String CONTENT, String EMAIL, Integer CLUB_NUM, Integer INTEREST_NUM, Integer MEMBER_COUNT) {
-        this.CLUB_NAME = CLUB_NAME;
-        this.INTRO = INTRO;
-        this.AREA_NUM = AREA_NUM;
-        this.CONTENT = CONTENT;
-        this.EMAIL = EMAIL;
-        this.NICKNAME = NICKNAME;
-        this.CLUB_NUM = CLUB_NUM;
-        this.INTEREST_NUM = INTEREST_NUM;
-        this.MEMBER_COUNT = MEMBER_COUNT;
+    public ClubBean CLUB_NUM(Integer CLUB_NUM) {
+        setCLUB_NUM(CLUB_NUM);
+        return this;
     }
 
     public ClubBean CLUB_NAME(String CLUB_NAME) {
-        this.CLUB_NAME = CLUB_NAME;
+        setCLUB_NAME(CLUB_NAME);
         return this;
     }
 
     public ClubBean INTRO(String INTRO) {
-        this.INTRO = INTRO;
+        setINTRO(INTRO);
         return this;
     }
 
-    public ClubBean AREA_NAME(Integer AREA_NUM) {
-        this.AREA_NUM = AREA_NUM;
+    public ClubBean AREA_NUM(Integer AREA_NUM) {
+        setAREA_NUM(AREA_NUM);
         return this;
     }
 
     public ClubBean CONTENT(String CONTENT) {
-        this.CONTENT = CONTENT;
+        setCONTENT(CONTENT);
         return this;
     }
 
     public ClubBean EMAIL(String EMAIL) {
-        this.EMAIL = EMAIL;
-        return this;
-    }
-
-    public ClubBean CLUB_NUM(Integer CLUB_NUM) {
-        this.CLUB_NUM = CLUB_NUM;
+        setEMAIL(EMAIL);
         return this;
     }
 
     public ClubBean NICKNAME(String NICKNAME) {
-        this.NICKNAME = NICKNAME;
+        setNICKNAME(NICKNAME);
+        return this;
+    }
+
+    public ClubBean TIME(String TIME) {
+        setTIME(TIME);
         return this;
     }
 
     public ClubBean INTEREST_NUM(Integer INTEREST_NUM) {
-        this.INTEREST_NUM = INTEREST_NUM;
+        setINTEREST_NUM(INTEREST_NUM);
         return this;
     }
 
     public ClubBean MEMBER_COUNT(Integer MEMBER_COUNT) {
-        this.MEMBER_COUNT = MEMBER_COUNT;
+        setMEMBER_COUNT(MEMBER_COUNT);
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof ClubBean)) {
+            return false;
+        }
+        ClubBean clubBean = (ClubBean) o;
+        return Objects.equals(CLUB_NUM, clubBean.CLUB_NUM) && Objects.equals(CLUB_NAME, clubBean.CLUB_NAME) && Objects.equals(INTRO, clubBean.INTRO) && Objects.equals(AREA_NUM, clubBean.AREA_NUM) && Objects.equals(CONTENT, clubBean.CONTENT) && Objects.equals(EMAIL, clubBean.EMAIL) && Objects.equals(NICKNAME, clubBean.NICKNAME) && Objects.equals(TIME, clubBean.TIME) && Objects.equals(INTEREST_NUM, clubBean.INTEREST_NUM) && Objects.equals(MEMBER_COUNT, clubBean.MEMBER_COUNT);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(CLUB_NUM, CLUB_NAME, INTRO, AREA_NUM, CONTENT, EMAIL, NICKNAME, TIME, INTEREST_NUM, MEMBER_COUNT);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " CLUB_NUM='" + getCLUB_NUM() + "'" +
+            ", CLUB_NAME='" + getCLUB_NAME() + "'" +
+            ", INTRO='" + getINTRO() + "'" +
+            ", AREA_NUM='" + getAREA_NUM() + "'" +
+            ", CONTENT='" + getCONTENT() + "'" +
+            ", EMAIL='" + getEMAIL() + "'" +
+            ", NICKNAME='" + getNICKNAME() + "'" +
+            ", TIME='" + getTIME() + "'" +
+            ", INTEREST_NUM='" + getINTEREST_NUM() + "'" +
+            ", MEMBER_COUNT='" + getMEMBER_COUNT() + "'" +
+            "}";
     }
 }
