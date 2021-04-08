16301f17ad545732d0b55fa4f0986f0a132fb58c
chaelin1211 <cofls749@naver.com>
2021-04-08 21:47:22 +0900

    change column name and sort query

--- src/main/resources/Udong_20190827_39_12.sql
+++ src/main/resources/Udong_20190827_39_12.sql
@@ -3,22 +3,16 @@
 -- CLUB Table Create SQL
 CREATE TABLE CLUB
 (
-    `CLUB_NAME`  VARCHAR
-(45)    NULL        COMMENT '동아리명', 
-    `INTRO`      VARCHAR
-(45)    NULL        COMMENT '동아리 소개', 
-    `AREA_NAME`  VARCHAR
-(45)    NULL        COMMENT '활동 지역명', 
-    `CONTENT`    VARCHAR
-(45)    NULL        COMMENT '활동내용', 
-    `ID`         VARCHAR
-(45)    NULL        COMMENT '팀장 아이디', 
-    `CLUB_NUM`   INT            NULL        COMMENT '동아리 번호', 
-    `INTEREST`   VARCHAR
-(45)    NULL        COMMENT '활동 주제', 
-    `RECOMMEND`  INT            NULL        COMMENT '추천 수', 
-    PRIMARY KEY
-(CLUB_NUM)
+    `CLUB_NAME`  VARCHAR(45)    NOT NULL        COMMENT '동아리명', 
+    `INTRO`      VARCHAR(45)    NOT NULL        COMMENT '동아리 소개', 
+    `AREA_NAME`  VARCHAR(45)    NOT NULL        COMMENT '활동 지역명', 
+    `CONTENT`    VARCHAR(45)    NOT NULL        COMMENT '활동내용', 
+    `EMAIL`      VARCHAR(45)     NOT NULL       COMMENT '팀장 이메일',
+    `Nickname`   VARCHAR(45)    NULL            COMMENT '팀장 별명',  
+    `CLUB_NUM`   INT            NOT NULL        AUTO_INCREMENT COMMENT '동아리 번호', 
+    `INTEREST`   VARCHAR(45)    NOT NULL        COMMENT '활동 주제', 
+    `RECOMMEND`  INT            NOT NULL        COMMENT '추천 수', 
+    PRIMARY KEY (CLUB_NUM)
 );
 
 ALTER TABLE CLUB COMMENT '동아리';
@@ -26,14 +20,14 @@ ALTER TABLE CLUB COMMENT '동아리';
 
 CREATE TABLE MEMBER
 (
-    `EMAIL`         VARCHAR(45)    NOT NULL    COMMENT '아이디', 
+    `EMAIL`      VARCHAR(45)    NOT NULL    COMMENT '아이디', 
     `NAME`       VARCHAR(45)    NULL        COMMENT '이름',  
-    `GENDER`       VARCHAR(45)    NULL        COMMENT '성별',  
-    `BIRTH_YEAR`       INT    NULL        COMMENT '출생연도',  
-    `BIRTH_MONTH`       INT    NULL        COMMENT '생일-월',  
-    `BIRTH_DAY`       INT    NULL        COMMENT '생일-일',  
-    `Nickname`       INT    NULL        COMMENT '별명',
-    PRIMARY KEY(ID)
+    `GENDER`     VARCHAR(45)    NULL        COMMENT '성별',  
+    `BIRTH_YEAR` INT            NULL        COMMENT '출생연도',  
+    `BIRTH_MONTH`INT            NULL        COMMENT '생일-월',  
+    `BIRTH_DAY`  INT            NULL        COMMENT '생일-일',  
+    `Nickname`   INT            NULL        COMMENT '별명',
+    PRIMARY KEY(EMAIL)
 );
 
 ALTER TABLE MEMBER COMMENT '멤버';
@@ -42,9 +36,9 @@ ALTER TABLE MEMBER COMMENT '멤버';
 -- CLUB Table Create SQL
 CREATE TABLE CLUB_MEMBER
 ( 
-    `EMAIL`     VARCHAR(45)    NOT NULL    COMMENT '멤버 아이디'
-    `CLUB_NUM`       INT            NOT NULL    AUTO_INCREMENT COMMENT '일련번호', 
-    PRIMARY KEY (CLUB_NUM)
+    `EMAIL`     VARCHAR(45)    NOT NULL     COMMENT '멤버 이메일'
+    `CLUB_NUM`       INT       NOT NULL     COMMENT '클럽 번호', 
+    PRIMARY KEY (EMAIL, CLUB_NUM)
 );
 
 ALTER TABLE CLUB_MEMBER COMMENT '동아리멤버';
@@ -53,12 +47,9 @@ ALTER TABLE CLUB_MEMBER COMMENT '동아리멤버';
 -- CLUB Table Create SQL
 CREATE TABLE AREA
 (
-    `AREA_NUM`   VARCHAR
-(45)    NULL        COMMENT '지역 번호', 
-    `AREA_NAME`  VARCHAR
-(45)    NULL        COMMENT '지역명', 
-    PRIMARY KEY
-(AREA_NUM)
+    `AREA_NUM`   VARCHAR(45)    NULL        COMMENT '지역 번호', 
+    `AREA_NAME`  VARCHAR(45)    NULL        COMMENT '지역명', 
+    PRIMARY KEY (AREA_NUM)
 );
 
 ALTER TABLE AREA COMMENT '지역';
@@ -67,11 +58,9 @@ ALTER TABLE AREA COMMENT '지역';
 -- CLUB Table Create SQL
 CREATE TABLE INTEREST_CATEGORY
 (
-    `NUM`   INT            NULL        COMMENT '분류 번호', 
-    `NAME`  VARCHAR
-(45)    NULL        COMMENT '이름', 
-    PRIMARY KEY
-(NUM)
+    `INTEREST_NUM`  INT            NULL        COMMENT '분류 번호', 
+    `NAME`          VARCHAR(45)    NULL        COMMENT '이름', 
+    PRIMARY KEY (INTEREST_NUM)
 );
 
 ALTER TABLE INTEREST_CATEGORY COMMENT '흥미분야';
@@ -80,99 +69,88 @@ ALTER TABLE INTEREST_CATEGORY COMMENT '흥미분야';
 -- CLUB Table Create SQL
 CREATE TABLE MEMBER_INTEREST
 (
-    `ID`    VARCHAR
-(45)    NULL        COMMENT '아이디', 
-    `NAME`  VARCHAR
-(45)    NULL        COMMENT '분류 이름', 
-    `NUM`   INT            NOT NULL    AUTO_INCREMENT COMMENT '일련번호', 
-    PRIMARY KEY
-(NUM)
+    `EMAIL` VARCHAR (45)    NOT NULL   COMMENT '이메일', 
+    `NAME`  VARCHAR (45)    NOT NULL   COMMENT '분류 이름'
+    PRIMARY KEY (EMAIL, NAME)
 );
 
 ALTER TABLE MEMBER_INTEREST COMMENT '멤버흥미';
 
 
--- CLUB Table Create SQL
+-- BOARD Table Create SQL
 CREATE TABLE BOARD
 (
     `POSTNUM`   INT            NOT NULL    AUTO_INCREMENT COMMENT '글번호', 
-    `CATEGORY`  VARCHAR(45)    NOT NULL        COMMENT '카테고리', 
-    `TIME`      TIMESTAMP      NOT NULL        COMMENT '작성일자', 
-    `TITLE`     VARCHAR(45)    NOT NULL        COMMENT '제목', 
-    `CONTENT`   VARCHAR(45)    NOT NULL        COMMENT '내용', 
-    `NAME`     VARCHAR(45)    NOT NULL    COMMENT '작성자',
+    `CATEGORY`  VARCHAR(45)    NOT NULL    COMMENT '카테고리', 
+    `TIME`      TIMESTAMP      NOT NULL    COMMENT '작성일자', 
+    `TITLE`     VARCHAR(45)    NOT NULL    COMMENT '제목', 
+    `CONTENT`   VARCHAR(45)    NOT NULL    COMMENT '내용', 
+    `NICKNAME`  VARCHAR(45)    NOT NULL    COMMENT '작성자 별명',
     `EMAIL`     VARCHAR(45)    NOT NULL    COMMENT '작성자 이메일',
-    `RECOMMEND`   INT    NULL        COMMENT '추천' 
-    PRIMARY KEY
-(POSTNUM)
+    `RECOMMEND` INT            NULL        COMMENT '추천' 
+    PRIMARY KEY (POSTNUM)
 );
 
 ALTER TABLE BOARD COMMENT '게시판';
 
 
--- CLUB Table Create SQL
+-- CATEGORY Table Create SQL
 CREATE TABLE CATEGORY
 (
     `NUM`   INT            NOT NULL    COMMENT '카테고리 번호', 
-    `NAME`  VARCHAR
-(45)    NULL        COMMENT '카테고리 이름', 
-    PRIMARY KEY
-(NAME)
+    `NAME`  VARCHAR(45)    NULL        COMMENT '카테고리 이름', 
+    PRIMARY KEY (NUM)
 );
 
 ALTER TABLE CATEGORY COMMENT '게시글 카테고리';
 
 
--- CLUB Table Create SQL
+-- FAQ Table Create SQL
 CREATE TABLE FAQ
 (
-    `NUM`      INT            NOT NULL    AUTO_INCREMENT COMMENT '글번호', 
-    `TITLE`    VARCHAR
-(45)    NULL        COMMENT '제목', 
-    `CONTENT`  VARCHAR
-(45)    NULL        COMMENT '내용', 
-    PRIMARY KEY
-(NUM)
+    `FAQ_NUM`       INT            NOT NULL    AUTO_INCREMENT COMMENT '글번호', 
+    `TITLE`         VARCHAR(45)    NULL        COMMENT '제목', 
+    `CONTENT`       VARCHAR(45)    NULL        COMMENT '내용', 
+    PRIMARY KEY (FAQ_NUM)
 );
 
 ALTER TABLE FAQ COMMENT 'FAQ';
 
 
--- CLUB Table Create SQL
+-- QNA Table Create SQL
 CREATE TABLE QNA
 (
-    `NUM`      INT            NULL        AUTO_INCREMENT COMMENT '질문 번호', 
-    `TIME`     TIMESTAMP      NULL        COMMENT '작성일자', 
-    `TITLE`    VARCHAR
-(45)    NULL        COMMENT '제목', 
-    `ID`       VARCHAR
-(45)    NULL        COMMENT '작성자', 
-    `CONTENT`  VARCHAR
-(45)    NULL        COMMENT '내용', 
-    `EMAIL`    VARCHAR
-(45)    NULL        COMMENT '이메일', 
-    PRIMARY KEY
-(NUM)
+    `NICKNAME`  VARCHAR(45)    NOT NULL    COMMENT '작성자 별명',
+    `EMAIL`     VARCHAR(45)    NOT NULL    COMMENT '작성자 이메일',
+    `QNA_NUM`   INT            NOT NULL    AUTO_INCREMENT COMMENT '질문 번호', 
+    `TIME`      TIMESTAMP      NOT NULL    COMMENT '작성일자', 
+    `TITLE`     VARCHAR(45)    NOT NULL    COMMENT '제목', 
+    `CONTENT`   VARCHAR(45)    NOT NULL    COMMENT '내용', 
+    PRIMARY KEY (QNA_NUM)
 );
 
 ALTER TABLE QNA COMMENT 'QNA';
 
+-- COMMENT Table Create SQL
 CREATE TABLE COMMENT
 (
-    `ID`        VARCHAR(45)    NULL        COMMENT '아이디', 
-    `POST_NUM`  INT    NULL        COMMENT '글번호', 
-    `CONTENT`   VARCHAR(45)    NULL        COMMENT '내용', 
-    `TIME`      TIMESTAMP      NULL        COMMENT '작성시간', 
-    `CO_NUM`    INT            NOT NULL    AUTO_INCREMENT COMMENT '댓글번호', 
+    `NICKNAME`  VARCHAR(45)   NOT NULL    COMMENT '작성자 별명',
+    `EMAIL`     VARCHAR(45)   NOT NULL    COMMENT '작성자 이메일',
+    `POST_NUM`  INT           NOT NULL    COMMENT '글번호', 
+    `CONTENT`   VARCHAR(45)   NOT NULL    COMMENT '내용', 
+    `TIME`      TIMESTAMP     NOT NULL    COMMENT '작성시간', 
+    `CO_NUM`    INT           NOT NULL    AUTO_INCREMENT COMMENT '댓글번호', 
     PRIMARY KEY (CO_NUM)
 );
 
 ALTER TABLE COMMENT COMMENT '댓글';
 
+-- RECOMMEND Table Create SQL
 CREATE TABLE RECOMMEND
 (
-    `ID`        VARCHAR(45)    NULL        COMMENT '아이디', 
-    `POST_NUM`  VARCHAR(45)    NULL        COMMENT '글번호'
+    `EMAIL`     VARCHAR(45)    NOT NULL    COMMENT '작성자 이메일',
+    `POST_NUM`  VARCHAR(45)    NOT NULL    COMMENT '글번호'
+    PRIMARY KEY (EMAIL, POST_NUM)
 );
 
 ALTER TABLE RECOMMEND COMMENT '추천';
