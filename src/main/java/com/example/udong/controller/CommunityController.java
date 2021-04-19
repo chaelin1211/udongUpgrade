b6ba07035b70f63ac020cb05c3946b5c8d2bf27e
chaelin1211 <cofls749@naver.com>
2021-04-19 12:04:43 +0900

    댓글 삭제를 위한 Controller 생성

--- src/main/java/com/example/udong/controller/CommunityController.java
+++ src/main/java/com/example/udong/controller/CommunityController.java
@@ -122,6 +122,32 @@ public class CommunityController {
         return "/view :: #commentTable";
     }
 
+    @RequestMapping(value = "/view/{action}", method = { RequestMethod.DELETE })
+    public String viewDeleteMethod(Model model, @PathVariable String action,
+            @RequestParam Map<String, Object> paramMap) {
+
+        CommentBean result = new CommentBean();
+        result.setCO_NUM(Integer.parseInt(paramMap.get("CO_NUM").toString()));
+        result.setPOST_NUM(Integer.parseInt(paramMap.get("POST_NUM").toString()));
+
+        // DB 댓글 삭제
+        commentservice.delete(result);
+
+        // 댓글 리스트 추가
+        model.addAttribute("commentList", commentservice.getList(result));
+
+        // 수정&삭제 버튼 게시를 위한 유저 정보 전달
+        Map<String, Object> userInform = new HashMap<String, Object>();
+        userInform.put("userEmail", paramMap.get("EMAIL"));
+        model.addAttribute("userInform", userInform);
+
+        // 화면 일반화를 위한 정보 전달
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("CATEGORY_NAME", paramMap.get("CATEGORY_NAME"));
+        model.addAttribute("resultMap", resultMap);
+        return "/view :: #commentTable";
+    }
+
     @RequestMapping(value = "/view", method = { RequestMethod.GET })
     public ModelAndView viewActionMethod(@RequestParam Map<String, Object> paramMap, ModelAndView modelAndView) {
 
