546469c53d50d69b697f956c2d0f43e853daa0ba
chaelin1211 <cofls749@naver.com>
2021-04-26 15:35:30 +0900

    동아리 검색을 위한 ajax 코드 추가 - js 파일 추가

---
+++ src/main/resources/static/js/club.js
@@ -0,0 +1,31 @@
+function searchClubLoaction(form, event){
+    event.preventDefault();
+
+    var areaName = form.AREA_NAME.value;
+
+    $.ajax({
+        url: "/club/search/location",
+        type: "GET",
+        data:{
+            AREA_NAME: areaName
+        }
+    }).done(function(fragment){
+        $("#clubList").replaceWith(fragment);
+    })
+}
+
+function searchClubInterest(form, event){
+    event.preventDefault();
+
+    var interestName = form.INTEREST_NAME.value;
+
+    $.ajax({
+        url: "/club/search/interest",
+        type: "GET",
+        data:{
+            INTEREST_NAME: interestName
+        }
+    }).done(function(fragment){
+        $("#clubList").replaceWith(fragment);
+    })
+}
