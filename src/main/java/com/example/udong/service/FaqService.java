824997064de29bdd5f0682c0e74667cd5cd909e5
chaelin1211 <cofls749@naver.com>
2021-05-04 07:52:58 +0900

    FAQ 페이지 출력 로직 변경

--- src/main/java/com/example/udong/service/FaqService.java
+++ src/main/java/com/example/udong/service/FaqService.java
@@ -1,6 +1,5 @@
 package com.example.udong.service;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,31 +10,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-public class FaqService{
+public class FaqService {
 
-    @Autowired
-    private FaqRepository repository;
-    
-    @Autowired
-    private CommonDao commonDao;
+  @Autowired
+  private FaqRepository repository;
 
+  @Autowired
+  private CommonDao commonDao;
 
-    public Object getList(Object dataMap) {
-		String sqlMapId = "Faq.list";
+  public Object getList(Object dataMap) {
+    String sqlMapId = "Faq.getAll";
 
-    Object resultObject = commonDao.getList(sqlMapId,dataMap);
-		// Object resultObject = new HashMap<>();
-		// ((Map<String, Object>) resultObject).put("resultList", dao.getList(sqlMapId,
-		// dataMap));
-		// ((Map<String, Object>) resultObject).put("resultList", repository.findAll());
+    Object resultObject = commonDao.getList(sqlMapId, dataMap);
+    // Object resultObject = new HashMap<>();
+    // ((Map<String, Object>) resultObject).put("resultList", dao.getList(sqlMapId,
+    // dataMap));
+    // ((Map<String, Object>) resultObject).put("resultList", repository.findAll());
 
-		return resultObject;
-    }
-    
-    public Object getObject(Object dataMap){
-        String sqlMapid = "Faq.read";
-        Object resultObject = commonDao.getObject(sqlMapid,dataMap);
-        return resultObject;
-    }
-    
+    return resultObject;
+  }
+
+  // public Object getObject(Object dataMap) {
+  //   String sqlMapid = "Faq.get";
+  //   Object resultObject = commonDao.getObject(sqlMapid, dataMap);
+  //   return resultObject;
+  // }
+
 }
