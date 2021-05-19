788f5ca446e3fe25c838ac59dedb7662de3c1eed
chaelin1211 <cofls749@naver.com>
2021-05-20 01:58:01 +0900

    email 컬럼 삭제로 인한 수정과 로그인 전 수행 제한을 위한 에러 처리

--- src/main/resources/static/js/recommend.js
+++ src/main/resources/static/js/recommend.js
@@ -1,39 +1,40 @@
 
 // 추천 버튼 클릭
-function recommendFunction(form, event){
+function recommendFunction(form, event) {
     event.preventDefault();
     var isRecommend = form.IsRecommend.value;
     var postNum = form.POST_NUM.value;
-    var email = form.userEmail.value;
     var recommend = form.recommend.value;
 
-    if(isRecommend == 0){
-        addRecommend(email, postNum, recommend);
-    }else{
-        deleteRecommend(email, postNum, recommend);
+    if (isRecommend == 0) {
+        addRecommend(postNum, recommend);
+    } else {
+        deleteRecommend(postNum, recommend);
     }
 }
 
-function addRecommend(email, postNum, recommend){
+function addRecommend(postNum, recommend) {
     $.ajax({
         url: "/view/recommend/PUT",
         type: "PUT",
         data: {
-            EMAIL: email,
             POST_NUM: postNum,
             RECOMMEND: recommend
         },
-    }).done(function (fragment) {
-        $('#recommendField').replaceWith(fragment);
-    });
+        error: function () {
+            alert("로그인 후에 추천 가능합니다.");
+        },
+        success: function (fragment) {
+            $('#recommendField').replaceWith(fragment);
+        }
+    })
 }
 
-function deleteRecommend(email, postNum, recommend){
+function deleteRecommend(postNum, recommend) {
     $.ajax({
         url: "/view/recommend/DELETE",
         type: "DELETE",
         data: {
-            EMAIL: email,
             POST_NUM: postNum,
             RECOMMEND: recommend
         },
