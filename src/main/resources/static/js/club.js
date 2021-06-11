438b7cd9d3e9d15d1d091edc09e0a62a7f1fdda4
chaelin1211 <cofls749@naver.com>
2021-06-11 23:18:47 +0900

    function 추가: 동아리 암호 필드 disable 토글

--- src/main/resources/static/js/club.js
+++ src/main/resources/static/js/club.js
@@ -1,4 +1,4 @@
-function searchClubLoaction(form, event){
+function searchClubLoaction(form, event) {
     event.preventDefault();
 
     var areaName = form.AREA_NAME.value;
@@ -6,15 +6,15 @@ function searchClubLoaction(form, event){
     $.ajax({
         url: "/club/search/location",
         type: "GET",
-        data:{
+        data: {
             AREA_NAME: areaName
         }
-    }).done(function(fragment){
+    }).done(function (fragment) {
         $("#clubList").replaceWith(fragment);
     })
 }
 
-function searchClubInterest(form, event){
+function searchClubInterest(form, event) {
     event.preventDefault();
 
     var interestName = form.INTEREST_NAME.value;
@@ -22,10 +22,20 @@ function searchClubInterest(form, event){
     $.ajax({
         url: "/club/search/interest",
         type: "GET",
-        data:{
+        data: {
             INTEREST_NAME: interestName
         }
-    }).done(function(fragment){
+    }).done(function (fragment) {
         $("#clubList").replaceWith(fragment);
     })
+}
+
+function checkBoxFunction() {
+    var inputField = document.getElementById("PINField");
+    var checkBoxValue = document.getElementById("pinCheckField");
+    if(checkBoxValue.checked == true){
+        inputField.disabled = false;
+    }else {
+        inputField.disabled = true;
+    }
 }
